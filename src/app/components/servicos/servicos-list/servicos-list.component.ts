8d1f8431aa0b0f4046123beb77e8b388a0558922
Patryk Rennan <165728048+emayceesh@users.noreply.github.com>
2025-04-11 00:35:47 -0300

    commit busca personalizada servicos

--- src/app/components/servicos/servicos-list/servicos-list.component.ts
+++ src/app/components/servicos/servicos-list/servicos-list.component.ts
@@ -2,21 +2,30 @@ import { Component, inject } from '@angular/core';
 import { Servicos } from '../../../models/servicos';
 import { ServicosService } from '../../../services/servicos.service';
 import Swal from 'sweetalert2';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-servicos-list',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './servicos-list.component.html',
   styleUrl: './servicos-list.component.scss'
 })
 export class ServicosListComponent {
+  
   lista: Servicos[] = [];
+  nomeFiltro!: string;
+  disponibilidadeFiltro!: string;
 
   servicosService = inject(ServicosService);
 
   constructor() {
     this.findAll();
+  }
+
+  ngOnInit(): void {
+    this.buscarServicosPorNome();
   }
 
   findAll(){
@@ -45,5 +54,38 @@ export class ServicosListComponent {
 
     }
   }
+
+  buscarServicosPorNome() {
+    if (this.nomeFiltro) {
+      this.servicosService.findByNome(this.nomeFiltro).subscribe((res) => {
+        this.lista = res;
+      });
+    } else {
+      this.servicosService.findAll().subscribe((res) => {
+        this.lista = res;
+      });
+    }
+  }
+
+  buscarServicos(): void {
+    if (this.nomeFiltro && this.disponibilidadeFiltro !== '') {
+      this.servicosService.findByNome(this.nomeFiltro).subscribe(res => {
+        this.lista = res.filter(s => s.disponivel.toString() === this.disponibilidadeFiltro);
+      });
+    } else if (this.nomeFiltro) {
+      this.servicosService.findByNome(this.nomeFiltro).subscribe(res => {
+        this.lista = res;
+      });
+    } else if (this.disponibilidadeFiltro !== '') {
+      this.servicosService.findByDisponibilidade(this.disponibilidadeFiltro === 'true').subscribe(res => {
+        this.lista = res;
+      });
+    } else {
+      this.servicosService.findAll().subscribe(res => {
+        this.lista = res;
+      });
+    }
+  }
+
 
 }
