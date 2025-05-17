dd19d3ee6f304616cae81de3ea940a8d4f13f03c
Patryk Rennan <165728048+emayceesh@users.noreply.github.com>
2025-05-16 22:35:37 -0300

    commit autorizacoes ja eraaaaa

--- src/app/components/layout/dashboard/dashboard.component.ts
+++ src/app/components/layout/dashboard/dashboard.component.ts
@@ -1,24 +1,77 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ChartData, ChartOptions } from 'chart.js';
+import { NgChartsModule } from 'ng2-charts';
+import { AgendamentoService } from '../../../services/agendamentos.service';
+import { Router } from '@angular/router';
+import { Cliente } from '../../../models/cliente';
+import { ClienteService } from '../../../services/cliente.service';
+import { AnimaisService } from '../../../services/animais.service';
+import { ViewChild } from '@angular/core';
+import { AnimaisListComponent } from '../../animais/animais-list/animais-list.component';
+import { LoginService } from '../../../auth/login.service';
+
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [NgChartsModule, AnimaisListComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+  
+  @ViewChild(AnimaisListComponent) animaisListComponent!: AnimaisListComponent;
 
-  listaGraficos: any = [];
+  agendamentoService = inject(AgendamentoService);
+  clienteService = inject(ClienteService);
+  animaisService = inject(AnimaisService);
+  loginService = inject(LoginService);
 
+  router = inject(Router);
 
-  constructor(){
-    this.listaGraficos.push("01");
-    this.listaGraficos.push("02");
-    this.listaGraficos.push("03");
-    this.listaGraficos.push("04");
-    this.listaGraficos.push("05");
-    this.listaGraficos.push("06");
+  agendamentosTotal: number = 0;
+  clientesTotal: number = 0;
+  animaisTotal: number = 0;
+
+  barChartLabels: string[] = [];
+  barChartData: ChartData<'bar'> = {
+    labels: [],
+    datasets: [
+      { data: [], label: 'Mais Agendados' }
+    ]
+  };
+  barChartOptions: ChartOptions = {
+    responsive: true,
+    scales: {
+      y: { beginAtZero: true }
+    }
+  };
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.agendamentoService.findAll().subscribe(agendamentos => {
+      this.agendamentosTotal = agendamentos.length;
+    });
+
+    this.clienteService.findAll().subscribe(clientes => {
+      this.clientesTotal = clientes.length;
+    });
+
+    this.animaisService.findAll().subscribe(animais => {
+      this.animaisTotal = animais.length;
+    });
   }
 
+  irParaNovoCliente() {
+    this.router.navigate(['/admin/cliente/new']);
+  }
+
+  irParaAgendamento() {
+    this.router.navigate(['/admin/agendamento']);
+  }
+  abrirModalAnimal() {
+    this.animaisListComponent.new(); 
+  }
 }
